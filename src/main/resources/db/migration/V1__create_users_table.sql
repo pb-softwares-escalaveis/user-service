328644e5fb9036000f5af5255d0d76df055a8444
Larissa Conti <laricbarros@gmail.com>
2026-05-17 22:58:57 -0300

    acrescentar status à tabela

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -5,11 +5,13 @@ CREATE SCHEMA IF NOT EXISTS usuario;
 CREATE SEQUENCE IF NOT EXISTS usuario.usuario_seq START WITH 1 INCREMENT BY 1;
 
 -- Criar a tabela no schema novo
-CREATE TABLE IF NOT EXISTS usuario.tb_users (
-                                               id BIGINT PRIMARY KEY,
-                                               name VARCHAR(255) NOT NULL,
-                                               email VARCHAR(255) UNIQUE NOT NULL,
-                                               cpf VARCHAR(255) NOT NULL,
-                                               marks INTEGER NOT NULL,
-                                               created_at TIMESTAMPTZ DEFAULT NOW()
+CREATE TABLE IF NOT EXISTS usuario.tb_users
+(
+    id         BIGINT PRIMARY KEY,
+    name       VARCHAR(255)        NOT NULL,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    cpf        VARCHAR(255)        NOT NULL,
+    marks      INTEGER             NOT NULL DEFAULT 3,
+    status     VARCHAR(50)         NOT NULL DEFAULT 'ACTIVE',
+    created_at TIMESTAMPTZ                  DEFAULT NOW()
 );
