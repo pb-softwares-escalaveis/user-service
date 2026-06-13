4109d3af53611c688e46f7f16600e58d2ba2938e
Larissa Conti <laricbarros@gmail.com>
2026-06-12 21:49:33 -0300

    anotação do schedule faltante

--- src/main/java/com/br/infnet/userservice/UserServiceApplication.java
+++ src/main/java/com/br/infnet/userservice/UserServiceApplication.java
@@ -3,13 +3,13 @@ package com.br.infnet.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class UserServiceApplication {
-
     static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
-
 }
